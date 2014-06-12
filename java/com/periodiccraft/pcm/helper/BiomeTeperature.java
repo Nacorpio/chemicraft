5ed21350abbe071632c395fb65116169fb0d112a
Victorious3 <Minefoto0@gmail.com>
2014-06-12 11:31:07 +0200

    Biome Temperature

---
+++ java/com/periodiccraft/pcm/helper/BiomeTeperature.java
@@ -0,0 +1,15 @@
+package com.periodiccraft.pcm.helper;
+
+public class BiomeTeperature 
+{
+	public static float getDefaultTemperature()
+	{
+		return 20F;
+	}
+	
+	public static float getTemperature(int biomeID)
+	{
+		//TODO implement that.
+		return getDefaultTemperature();
+	}
+}
