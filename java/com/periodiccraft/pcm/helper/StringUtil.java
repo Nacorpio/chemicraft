a1cb6ab4da7b2bbb24e10de0a86bb4814d3b3bc2
Nacorpio <teampavilion@gmail.com>
2014-06-12 15:32:12 +0200

    Added some new utilities for the StringUtil.

--- java/com/periodiccraft/pcm/helper/StringUtil.java
+++ java/com/periodiccraft/pcm/helper/StringUtil.java
@@ -2,6 +2,17 @@ package com.periodiccraft.pcm.helper;
 
 public final class StringUtil {
 
+	public static String getBetween(String raw, String str1, String str2) {
+		
+		String result = "";
+		
+		int str1Index = raw.indexOf(str1) + str1.length() - 1;
+		int str2Index = raw.indexOf(str2) - 1;
+		
+		return raw.substring(str1Index, str2Index);
+		
+	}
+	
 	public static String getLongestString(String[] par1) {
 		String result = "";
 		int longest = 0;
@@ -11,6 +22,35 @@ public final class StringUtil {
 				result = var1;
 				longest = var1.length();
 				continue;
+			} else if (var1.length() == longest) {
+				result = var1;
+				longest = var1.length();
+			}
+		}
+		return result;
+	}
+	
+	public static String[] getStringsLongerThan(String[] par1, int par2) {
+		String[] result = new String[]{};
+		int latestLong = 0;
+		for (int i = 0; i < par1.length; i++) {
+			String var1 = par1[i];
+			if (var1.length() > par2) {
+				latestLong = var1.length();
+				result[result.length] = var1;
+			}
+		}
+		return result;
+	}
+	
+	public static String[] getStringsShorterThan(String[] par1, int par2) {
+		String[] result = new String[]{};
+		int latestShort = 0;
+		for (int i = 0; i < par1.length; i++) {
+			String var1 = par1[i];
+			if (var1.length() < par2) {
+				latestShort = var1.length();
+				result[result.length] = var1;
 			}
 		}
 		return result;
@@ -25,6 +65,9 @@ public final class StringUtil {
 				result = var1;
 				shortest = var1.length();
 				continue;
+			} else if (var1.length() == shortest) {
+				result = var1;
+				shortest = var1.length();
 			}
 		}
 		return result;
