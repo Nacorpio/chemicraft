ce064813da376403fec0b3216be96533c5e948fa
Andy <solarcactus@live.com>
2014-06-28 11:43:59 -0700

    Some more Ph

--- java/com/periodiccraft/pcm/core/element/pH.java
+++ java/com/periodiccraft/pcm/core/element/pH.java
@@ -7,12 +7,21 @@ public class pH {
 	
  /*
   * Make sure you remember what pH stands for. Potential of Hydrogen.
-  * We should add Ph to the research.
-  * TODO the above thing.
+  * TODO We should add Ph to the research.
   * 
   */
 
-
+	private double pHlevel; //0-8.1
 
 	
+	public boolean isHarmful() {
+		if(pHlevel >= 6.81 || pHlevel <= 6.81) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+		
 }
+	
+	
