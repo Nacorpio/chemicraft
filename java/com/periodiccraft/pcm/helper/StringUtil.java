840d06f33f702f1f34d7e558e0a960955a0178fe
Nils Andreas Svee <nils.a.svee@gmail.com>
2014-06-28 23:39:27 +0200

    Use foreach loops instead as they're more readable

--- java/com/periodiccraft/pcm/helper/StringUtil.java
+++ java/com/periodiccraft/pcm/helper/StringUtil.java
@@ -16,60 +16,54 @@ public final class StringUtil {
 	public static String getLongestString(String[] par1) {
 		String result = "";
 		int longest = 0;
-		for (int i = 0; i < par1.length; i++) {
-			String var1 = par1[i];
-			if (var1.length() > longest) {
-				result = var1;
-				longest = var1.length();
-				continue;
-			} else if (var1.length() == longest) {
-				result = var1;
-				longest = var1.length();
-			}
-		}
+        for (String var1 : par1) {
+            if (var1.length() > longest) {
+                result = var1;
+                longest = var1.length();
+            } else if (var1.length() == longest) {
+                result = var1;
+                longest = var1.length();
+            }
+        }
 		return result;
 	}
 	
 	public static String[] getStringsLongerThan(String[] par1, int par2) {
 		String[] result = new String[]{};
 		int latestLong = 0;
-		for (int i = 0; i < par1.length; i++) {
-			String var1 = par1[i];
-			if (var1.length() > par2) {
-				latestLong = var1.length();
-				result[result.length] = var1;
-			}
-		}
+        for (String var1 : par1) {
+            if (var1.length() > par2) {
+                latestLong = var1.length();
+                result[result.length] = var1;
+            }
+        }
 		return result;
 	}
 	
 	public static String[] getStringsShorterThan(String[] par1, int par2) {
 		String[] result = new String[]{};
 		int latestShort = 0;
-		for (int i = 0; i < par1.length; i++) {
-			String var1 = par1[i];
-			if (var1.length() < par2) {
-				latestShort = var1.length();
-				result[result.length] = var1;
-			}
-		}
+        for (String var1 : par1) {
+            if (var1.length() < par2) {
+                latestShort = var1.length();
+                result[result.length] = var1;
+            }
+        }
 		return result;
 	}
 	
 	public static String getShortestString(String[] par1) {
 		String result = "";
 		int shortest = Integer.MAX_VALUE;
-		for (int i = 0; i < par1.length; i++) {
-			String var1 = par1[i];
-			if (var1.length() < shortest) {
-				result = var1;
-				shortest = var1.length();
-				continue;
-			} else if (var1.length() == shortest) {
-				result = var1;
-				shortest = var1.length();
-			}
-		}
+        for (String var1 : par1) {
+            if (var1.length() < shortest) {
+                result = var1;
+                shortest = var1.length();
+            } else if (var1.length() == shortest) {
+                result = var1;
+                shortest = var1.length();
+            }
+        }
 		return result;
 	}
 	
